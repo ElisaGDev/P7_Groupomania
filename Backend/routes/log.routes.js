344aef47ee439c8074d6f71cc5208f68e875a884
ElisaGDev <100610528+ElisaGDev@users.noreply.github.com>
2022-08-02 19:22:32 +0200

    Create follow unfollow

--- Backend/routes/log.routes.js
+++ Backend/routes/log.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const logCtrl = require("../controllers/log.controller");
 
+//Log
 router.post("/register", logCtrl.register);
 router.post("/login", logCtrl.login);
 router.get("/logout", logCtrl.logOut);
