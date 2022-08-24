4af96b34d194e4df4099a683b5de32cde6215eeb
ElisaGDev <100610528+ElisaGDev@users.noreply.github.com>
2022-08-24 18:03:50 +0200

    Thread

--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/post.controller");
 const multer = require("multer");
+const upload = multer({ dest: "uploads/posts" });
 
 //Posts
 router.get("/", postController.getPosts);
 router.get("/:id", postController.getOnePost);
-router.post("/", postController.createPost);
+router.post("/", upload.single("file"), postController.createPost);
 router.put("/:id", postController.updatePost);
 router.delete("/:id", postController.deletePost);
 
