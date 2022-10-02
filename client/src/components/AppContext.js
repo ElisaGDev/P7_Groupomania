ec9250c0dc16c1037dddfbd33e7fc2262d215763
ElisaGDev <100610528+ElisaGDev@users.noreply.github.com>
2022-10-02 14:47:23 +0200

    modif style

--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -1,3 +1,5 @@
+// Permet de transmettre des données sans avoir à le faire manuellement et à chaque fois
 import { createContext } from "react";
 
+// Uid = identifiant unique
 export const UidContext = createContext();
